cd7524125a0545bb79b77788cb5b8c27d602aa5f
Jemaine Brown <jembrown@jemaines-mbp.attlocal.net>
2018-06-08 19:40:02 -0500

    Setup server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP());
 
 const port = 3080;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
